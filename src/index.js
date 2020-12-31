ba5a300d5854878a833fbf741775f9a6e6b2595b
Andrey Ageev <ageev@MacBook-Pro-Andrey.local>
2020-12-31 14:35:08 +0300

    1-index done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+  return new Promise((resolve => {
+    setTimeout(() => {
+      resolve()
+    }, seconds*1000);
+  }))
 }
 
 /*
@@ -25,6 +30,13 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+  return fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+    .then(towns => towns.json())  
+    .then(towns => towns.sort(function (a, b) {
+      if (a.name > b.name) return 1
+      if (a.name < b.name) return -1
+      return 0
+    }))
 }
 
 export {
